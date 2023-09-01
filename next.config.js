769ed7b260c8a3ba37853ddb2f61139defad7ee2
danuwardanaaa <danuwardana@student.telkomuniversity.ac.id>
2023-09-02 01:20:28 +0700

    perbaikan

--- next.config.js
+++ next.config.js
@@ -1,14 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    formats : ['image/avif' , 'image/webp'],
-    remotePatterns : [
-        {
-            protocol : 'https',
-            hostname : 'assets.vercel.com',
-            port : '',
-            pathname : '/public/img/**',
-        }
-    ]
+    // formats : ['image/avif' , 'image/webp'],
+    // remotePatterns : [
+    //     {
+    //         protocol : 'https',
+    //         hostname : 'assets.vercel.com',
+    //         port : '',
+    //         pathname : '/public/img/**',
+    //     }
+    // ]
 //   basePath: "/sehatku-healthcare-AI",
 //   assetPrefix: "/sehatku-healthcare-AI/",
 //   output: "export",
